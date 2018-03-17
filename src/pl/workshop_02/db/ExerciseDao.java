038ffe5acfb8234e3075dc454bdc5029df7a76c4
MarcinHi <mr.chaj@gmail.com>
2018-03-17 19:51:44 +0100

    small constructors corrections and improved code readability

--- src/pl/workshop_02/db/ExerciseDao.java
+++ src/pl/workshop_02/db/ExerciseDao.java
@@ -12,10 +12,12 @@ public class ExerciseDao {
 	private int id;
 	private String title;
 	private String description;
-	
+
 	public ExerciseDao() {
 	}
-	public ExerciseDao(String title, String description) {
+
+	public ExerciseDao(int id, String title, String description) {
+		this.id = id;
 		this.title = title;
 		this.description = description;
 	}
@@ -23,30 +25,30 @@ public class ExerciseDao {
 	public int getId() {
 		return id;
 	}
-	
+
 	public String getTitle() {
 		return title;
 	}
-	
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
-	
+
 	public String getDescription() {
 		return description;
 	}
-	
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
+
 	static public List<ExerciseDao> loadAllExercises(Connection conn) throws SQLException {
 		String sql = "SELECT * FROM exercise";
 		List<ExerciseDao> exercises = new ArrayList<>();
 		PreparedStatement preStm = conn.prepareStatement(sql);
 		ResultSet rs = preStm.executeQuery();
 		while (rs.next()) {
-			ExerciseDao loadedExercise= new ExerciseDao();
+			ExerciseDao loadedExercise = new ExerciseDao();
 			loadedExercise.id = rs.getInt("id");
 			loadedExercise.title = rs.getString("title");
 			loadedExercise.description = rs.getString("description");
@@ -104,17 +106,17 @@ public class ExerciseDao {
 			this.id = 0;
 		}
 	}
-	
-	public List<SolutionDao> loadAllSolutionsByUserId(int id, Connection conn) throws SQLException{
+
+	public List<SolutionDao> loadAllSolutionsByUserId(int id, Connection conn) throws SQLException {
 		List<SolutionDao> list = new ArrayList<>();
 		String sql = "SELECT * FROM solution WHERE users_id = ? AND exercise_id = ?";
 		PreparedStatement preStm = conn.prepareStatement(sql);
 		preStm.setInt(1, id);
 		preStm.setInt(2, this.id);
 		ResultSet rs = preStm.executeQuery();
-		while(rs.next()) {
+		while (rs.next()) {
 			SolutionDao loadedSolution = new SolutionDao();
-			loadedSolution.setId(rs.getInt("id"));
+			loadedSolution.setSolutionId(rs.getInt("id"));
 			loadedSolution.setCreated(rs.getString("created"));
 			loadedSolution.setUpdated(rs.getString("updated"));
 			loadedSolution.setDescription("description");
